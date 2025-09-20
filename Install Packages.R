222afd22586720416d5236ee00e72d4e48474902
LinesOnClay <134740923+LinesOnClay@users.noreply.github.com>
2025-09-20 08:03:31 -0700

    Install Packages.R

---
+++ Install Packages.R
@@ -0,0 +1,5 @@
+# Install and load necessary packages
+
+install.packages(c("quantmod", "ggplot2", "dplyr", "tidyr", "xts","TTR","lubridate","RColorBrewer","stringr","PerformanceAnalytics","scales","zoo","googlesheets4","readr","gt"))
+
+
